3bb1ad0253bdeeffd5f26e5486d3c068b6351986
Tre-boop <72523084+Tre-boop@users.noreply.github.com>
2020-10-14 22:41:05 -0400

    Final Copy

--- 4410352cfb89bbd56e1eab82c351ace4-c8efd5cc95b5a1933cedb56a7e6b04db332fe43c/Hauntedhouse.java
+++ 4410352cfb89bbd56e1eab82c351ace4-c8efd5cc95b5a1933cedb56a7e6b04db332fe43c/Hauntedhouse.java
@@ -18,7 +18,7 @@ class Hauntedhouse {
                     next_room();
                 case 2:
                     System.out.println("door two opens");
-                    break;
+                    room_1();
                 case 3:
                     System.out.println("door three opens");
                     next_room3_3();
@@ -33,11 +33,149 @@ class Hauntedhouse {
 
     }
 
+    public static void room_1() {
+        System.out.println("You carefully open the door to the next room.");
+        System.out.println("You begin to see two strangely illuminating lights in the distance.");
+        System.out.println("As you tread forward you gain a closer look.\n it seems as though they are glowing eggs");
+        System.out.println("The first egg is has a faint golden glow and the second has a vibrant green glow.");
+        System.out.println("after careful observation you notice that there is a door containing a lock behind each egg ");
+        System.out.println("You now know you have to break one of the eggs to retrieve the key to continue.\nWhich egg do you break?1 or 2?");
+        Scanner scanner = new Scanner(System.in);
+        int eggs = scanner.nextInt();
+        if (eggs == 1) {
+            System.out.println("After breaking the egg you start to see a golden key emerge form it");
+            System.out.println("You then slide the key into the rusted lock.\n as the door creeps open you hear a grunting echoing towards you");
+        } else {
+            System.out.println("The green illuminating egg begins to hiss violently.");
+            System.out.println("Then all of a sudden a giant flesh eating snake lunges at you and you fall down.");
+            System.out.println("In a desperate attempt to escape you find a screw driver.\n fearing the snake will eat you alive you forcefully impale the snake's eye.");
+            System.out.println("how hard do you penetrate the snake? enter a number!");
+            int pierce = scanner.nextInt();
+            if (pierce == 1) {
+                System.out.println("the snake give off a screeching noise and becomes a corpse.");
+            } else {
+                int bite = 5;
+                for (int x = 0; x < bite; x++) {
+                    userHealth -= 1;
+                }
+                System.out.println("as you penetrate the snake with the screw driver it somehow manages to bite your arm");
+                System.out.println("your health is now:" + userHealth);
+                System.out.println("the snake then drops to the ground and leaves a a fang stuck in your arm.\n you use it to pick the lock to the second door.");
+
+            }
+        }
+        is_dead();
+        room_2();
+    }
+
+    public static void room_2() {
+        System.out.println("You open the door the all a sudden you hear a loud grunting noise.");
+        System.out.println("You start to see mangled humanoid right in front of you.");
+        System.out.println("Frighten by the sight you freeze in a panic.\nIn that brief moment you notice that goul like figure has no eyes.");
+        System.out.println("Thinking it will not notice you step forward and it stares into your eyes as if it senses your every movement.");
+        System.out.println("Behind the mangled being you see a door.\nYou know in order to get past it you have two options crawl past it or strike the goul down.?");
+        System.out.println("Which option do you choose 1 or 2");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        if (option == 1) {
+            System.out.println("you some how manage to sneak your way past the ghoul and force the door open as fast as you can.");
+
+        } else {
+            int scratch = 5;
+            for (int x = 0; x < scratch; x++) {
+                userHealth -= 1;
+            }
+            System.out.println("The ghoul swings its claws at you as you hit it with your fist.");
+            System.out.println("your health is now:" + userHealth);
+            System.out.println("the ghoul is now slain making the path to the door clear.");
+
+        }
+        is_dead();
+        room_3();
+    }
+    public static void is_dead() {
+        if (userHealth <= 0) {
+            System.out.println("you have died");
+            //throw new NullPointerException();
+            System.exit(11);
+        }
+    }
+
+    public static void room_3() {
+        System.out.println("You open the door in a hurry while entering the room you almost step on what seems like a pressure plate.");
+        System.out.println("you see two pressure plates and a door behind it");
+        System.out.println("on both plates you see different symbols which look like hieroglyphics.");
+        System.out.println("on the first pressure plate you see a line, bird, bowl, and line.");
+        System.out.println("on the second pressure plate you see a snail, raven, feathers, lion, bird and eye.");
+        System.out.println("you know if you step on the correct pressure plate the door will open.\n which pressure plate do you step on 1 or 2?");
+        Scanner scanner = new Scanner(System.in);
+        int pressure = scanner.nextInt();
+        if (pressure == 1) {
+            System.out.println("with a bit of force the plate slides into the ground causing the door to swing open.");
+            System.out.println("as you pass through the door you walk into a loud crunching noise as you take each step.");
+        } else {
+            System.out.println("the pressure plate slides into the ground then there is a brief pause.");
+            System.out.println("then all of a sudden the room starts rumbling. you start to see the floor opening.");
+            System.out.println("you stare into the bottomless pit the floor displays.\n fearing you might fall in you jump over it and land on the first plate.");
+            System.out.println("while jumping in the air the pit spews a sizzling dark black ooze onto your legs.");
+            System.out.println("fortunately it does not burn through your clothes leaving you burnt free but now your covered in dark ooze");
+            System.out.println("the door swings open and you encounter a maze of bones");
+        }
+        is_dead();
+        room_4();
+    }
+
+    public static void room_4() {
+        System.out.println("You open the door the all a sudden you hear a loud crunching noise as you step forward.");
+        System.out.println("You start to see a labyrinth of bone which seems to be leading to a door with empty case on it which says give offering.");
+        System.out.println("making you way through the challenging path of bones you discover two sugar skulls.");
+        System.out.println("the first skull has a decorated flower on it and the second as a scythe painted on it.");
+        System.out.println("you one of the skull needs to presented as an offering to the case.\n which skull do you offer 1 or 2 ");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        if (option == 1) {
+            System.out.println("you place the flower skull inside the case. the room gives off a rose aroma and the door opens.");
+        } else {
+            int bite = 5;
+            for (int x = 0; x < bite; x++) {
+                userHealth -= 1;
+            }
+            System.out.println("you place the scythe decorated skull in the case. the all of a sudden the bones merges into a four legged creature.");
+            System.out.println("the creature latches onto your arm causing a minor cut. luckily you kick it off of your arm and the creature crumbles into pieces.");
+            System.out.println("your health is now:" + userHealth);
+            System.out.println("the creature is now defeated. you replace the second skull with the first causing the door to slowly open.");
+        }
+        is_dead();
+        room_5();
+    }
+
+    public static void room_5() {
+        System.out.println("You open the door to the next room it is pitch black.\nYou manage to find a dim flashlight");
+        System.out.println("As you shine your light to clear a path way you discover bats sleeping bat hanging above you.");
+        System.out.println("Fearing you would wake them up you shine the light in front of you and you find out that there is a door with two levers on each side.");
+        System.out.println("Under each lever there is an engraving. the first one says abrir and the second says luz.");
+        System.out.println("Your curiosity tells you that one of these levers opens the door.\nWhich lever do you choose 1 or 2?");
+        Scanner scanner = new Scanner(System.in);
+        int lever = scanner.nextInt();
+        if (lever == 1) {
+            System.out.println("you forcefully push down the lever.\nThe door creeks open...");
+            System.out.println("you exit the door with great relief know you did not wake the bats.");
+        } else {
+            System.out.println("you push down the lever.then all of a sudden the room lights up.");
+            System.out.println("you start to hear hundreds of screeching noises.\nThen in a blink of an eye the bats swarm you.");
+            System.out.println("out of panic you shine your flash light at them.\nThe bats become stunned causing them to forcefully ram into the door.");
+            System.out.println("the bats force open the door and fly out of the room. knowing that you are free you run to the door and exit the room.");
+        }
+        is_dead();
+        System.exit(10);
+    }
+
     public static void next_room3_3() {
         System.out.println("As you open the third door you get a sinking feeling.");
         System.out.println("You are reluctant to look down, you look down and notice there is no floor.");
         System.out.println("you fall to your death");
         System.exit(4);
+        is_dead();
     }
 
     public static void next_room() {
@@ -63,6 +201,7 @@ class Hauntedhouse {
                     System.out.println("You hit them all at once how ever it doesn't full incapticate one.\n It has enough time to scratch you.");
                     System.out.println("You take 3 damage.");
                     System.out.println("your health is now:" + userHealth);
+                    is_dead();
                 }
             }
             System.out.println("Book case turns revealing two doors.\n A wave of relief passes over you.\n you walk to the doors for a closer look.");
@@ -107,6 +246,7 @@ class Hauntedhouse {
                 }
                 System.out.println("You swing the old mace but you feel it crack mid swing. You slowed your strike to account for this, The spider was able to reach you and is able to bite you in the leg. You take another swing and kill it this time.");
                 System.out.println("You have this amount of health remaining:" + userHealth);
+                is_dead();
             }
         }
         System.out.println("You look back in the first box seeing nothing was in there you moved on to the next box.\nBeing prepare this time you poke the box and out jumps a dead spider.\nYou grab the key and head over the tower of boxes.\nYou move them out the way and you head through the trap door.");
@@ -233,6 +373,7 @@ class Hauntedhouse {
                 System.out.println("You hit the machine and instantly feel a wave of regret as the machine starts to shake violently.");
                 System.out.println("The Machine starts to glow brilliantly and then explodes.\n after a bit of time you waken and notice the room is now in shambles.");
                 System.out.println("Miraculously you are okay but of course a bit ding up.\nYour health is now:" + userHealth);
+                is_dead();
                 System.out.println("As you gather your self you notice that a wall used to be there is now a stack of rumble.\nBehind it is two doors.\nThe first door is freezing to the touch and almost fells like metallic.");
                 System.out.println("The second door is rather small as if only a child could walk through it.\nWhich door do you choose 1 or 2?");
                 int finchoice = scanner.nextInt();
@@ -268,6 +409,7 @@ class Hauntedhouse {
         System.exit(8);
     }
 }
+
 
 
 
